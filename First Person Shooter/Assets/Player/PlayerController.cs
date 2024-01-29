e476cfd318fe30ab4c850e6f39a0dc86c0bc011a
JasonBock2159 <144266624+JasonBock2159@users.noreply.github.com>
2024-01-29 17:46:59 -0500

    Added Quake movement
    Added Quake movement

--- First Person Shooter/Assets/Player/PlayerController.cs
+++ First Person Shooter/Assets/Player/PlayerController.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
+    //Debug
+    public TMP_Text debug_text;
+    
     //Camera variables
     public Camera cam;
     private Vector2 look_input = Vector2.zero;
@@ -15,8 +19,28 @@ public class PlayerController : MonoBehaviour
     private int invert_x_factor = 1;
     private int invert_y_factor = 1;
     [Range(0.01f, 1f)]public float sensitivity;
-    
-    
+
+    //Player Input
+    private Vector2 move_input;
+    private bool grounded;
+
+    //Movement Variables
+    private CharacterController character_controller;
+    private Vector3 player_velocity;
+    private Vector3 wish_dir = Vector3.zero;
+    public float max_speed = 6;
+    public float accelerator = 60;
+    public float gravity = 20;
+    public float stop_speed= 0.5f;
+    public float jump_impulse = 10f;
+    public float friction = 4;
+
+
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +50,9 @@ public class PlayerController : MonoBehaviour
         //Invert Camera
         if (invert_x) invert_x_factor = -1;
         if(invert_y) invert_y_factor=-1;
+
+        //Get Components
+        character_controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -39,6 +66,16 @@ public class PlayerController : MonoBehaviour
     {
         look_input = context.ReadValue<Vector2>();
     }
+
+    public void GetMoveInput(InputAction.CallbackContext context)
+    {
+        move_input = context.ReadValue<Vector2>();
+    }
+
+    public void GetJumpInput(InputAction.CallbackContext context)
+    {
+        Jump();
+    }
     private void Look()
     {
         // Left Right Movement
@@ -50,4 +87,59 @@ public class PlayerController : MonoBehaviour
         horizontal_look_angle = Mathf.Clamp(horizontal_look_angle, -90, 90);
         cam.transform.localRotation = Quaternion.Euler(horizontal_look_angle,0,0);
     }
+
+    private void Jump()
+    {
+        if (grounded)
+        {
+            //Do this later
+        }
+    }
+
+    private Vector3 Accelerate(Vector3 wish_dir, Vector3 current_velocity, float accel, float max_speed)
+    {
+        float proj_speed = Vector3.Dot(current_velocity, wish_dir);
+        float accel_speed = accel * Time.deltaTime;
+
+        if (proj_speed + accel_speed > max_speed)
+            accel_speed = max_speed - proj_speed;
+        
+        
+        return current_velocity + (wish_dir * accel_speed);
+    }
+
+
+
+    private Vector3 MoveGround(Vector3 wish_dir, Vector3 current_velocity)
+    {
+        Vector3 new_velocity= new Vector3(current_velocity.x,0,current_velocity.z);
+
+        float speed= new_velocity.magnitude;
+        if(speed <= stop_speed)
+        {
+            new_velocity = Vector3.zero;
+            speed = 0;
+        }
+        if (speed != 0)
+        {
+            float drop =* friction * Time.deltaTime;
+            new_velocity *= Mathf.Max(speed - drop, 0) / speed;
+
+        }
+        new_velocity = new Vector3(new_velocity.x, current_velocity.y, new_velocity.z);
+        
+        
+        
+        return Accelerate(wish_dir, new_velocity, accelerator, max_speed);
+    }
+
+    private Vector3 MoveAir(Vector3 wish_dir, Vector3 current_velocity)
+    {
+       
+
+
+
+         return Accelerate(wish_dir, current_velocity, accelerator, max_speed);
+    }
 }
+
